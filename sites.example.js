c81df8c575e73d446e8bee0b3d7b6b4cc776bb7e
Mark Hatchell <mark.hatchell@prospect33.com>
2018-02-08 12:48:13 -0500

    added example site and url list

---
+++ sites.example.js
@@ -0,0 +1,53 @@
+exports.default = [
+    {
+        serverName: 'localhost - PHP',
+        tests: [
+            {
+                url: 'http://127.0.0.1:8080'
+            },
+            {
+                url: 'http://127.0.0.1:8080'
+            },
+            {
+                url: 'http://127.0.0.1:8080'
+            },
+            {
+                url: 'http://127.0.0.1:8080'
+            },
+        ]
+    },
+    {
+        serverName: 'localhost - PYTHON',
+        tests: [
+            {
+                url: 'http://127.0.0.1:8080'
+            },
+            {
+                url: 'http://127.0.0.1:8080'
+            },
+            {
+                url: 'http://127.0.0.1:8080'
+            },
+            {
+                url: 'http://127.0.0.1:8080'
+            },
+        ]
+    },
+    {
+        serverName: 'localhost - RUBY',
+        tests: [
+            {
+                url: 'http://127.0.0.1:8080'
+            },
+            {
+                url: 'http://127.0.0.1:8080'
+            },
+            {
+                url: 'http://127.0.0.1:8080'
+            },
+            {
+                url: 'http://127.0.0.1:8080'
+            },
+        ]
+    }
+];
